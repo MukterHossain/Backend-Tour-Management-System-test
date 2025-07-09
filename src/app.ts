29603e778c103ad5ac5a6c34055d34865af65958
MukterHossain <mukter3075@gmail.com>
2025-07-09 21:52:07 +0600

    added route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from "express";
 const app = express()
+import cors from "cors"
+import { UserRouters } from "./app/modules/user/user.route";
+
+app.use(express.json())
+app.use(cors())
+
+
+app.use("/api/v1/user/", UserRouters)
+
 
 
 app.get("/", (req:Request, res:Response) =>{
     res.status(200).json({
-        message: "Welcome to the Tour Management System API"
+        message: "Welcome to the Tour Management System API Test"
     })
 }) 
 
